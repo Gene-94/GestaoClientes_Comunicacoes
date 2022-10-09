83b165cca99e366c7cec378dc53248df80a8e5e2
yevheniy <yevheniykushnirenko@gmail.com>
2022-10-09 18:32:40 +0100

    Created draft of Client model and updated to database

---
+++ Data/Temp_MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Trabalho_Final.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trabalho_Final.Data
+{
+    public class Temp_MyDbContext : DbContext 
+    {
+        const string ConnectionStr = "server=localhost;port=3306;database=EmpresaComs;user=root;password=Malloc33";
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConnectionStr, new MySqlServerVersion(new Version(11, 3, 0)));
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Trabalho_Final.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int NrCliente { get; set; }
+        public string Nome { get; set; }
+        public string Morada { get; set; }
+        public string CodPostal { get; set; }
+        public string Localidade { get; set; }
+        public string Telefone { get; set; } 
+        public string Email { get; set; }
+        public string NrContribuinte { get; set; }
+        public float SaldoDisponivel { get; set; }
+        public DateTime ValidadeSaldo { get; set; }
+        // public Blob Avatar { get; set; }
+    }
+}
