1d8406e0130b328ad700c4a1f65a59c86cfe7aaa
yevheniy <yevheniykushnirenko@gmail.com>
2022-10-14 09:23:08 +0100

    Added models 'Cartao' & 'Menssagem'

---
+++ Models/Mensagem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trabalho_Final.Models
+{
+    public class Mensagem
+    {
+        [Key]
+        public int Id_Mensagem { get; set; }
+
+        [ForeignKey("Cartao")]
+        public int Origem { get; set; }
+
+        [ForeignKey("Cartao")]
+        public int Destino { get; set; }
+
+        public DateTime Data_Envio { get; set; }
+
+        public string Conteudo { get; set; }
+
+
+
+    }
+}
